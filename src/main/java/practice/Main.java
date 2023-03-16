420f37c255a13352569982c86bae9084fe2ce4bb
mamkinShcoder <Alexcoolarbitr@gmail.com>
2023-03-16 17:01:47 +0300

    Доделано)

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -7,79 +7,129 @@ public class Main {
         PhoneBook phoneBook = new PhoneBook();
         Scanner scanner = new Scanner(System.in);
 
-        for (;;){
-            System.out.println("Введите номер, имя или команду “LIST” для просмотра списка контактов:");
+        for (; ; ) {
+            System.out.println("Введите номер, имя, команду “LIST” для просмотра списка контактов" +
+                    " или “SEARCH” дял поиска контакта:");
             String firstInput = scanner.nextLine();
-            if (firstInput == "LIST"){
-                phoneBook.typePhoneBook();
+            if (firstInput.equals("LIST")) {
+                System.out.println(phoneBook.getAllContacts());
+                continue;
+            }
+            if (firstInput.equals("SEARCH")) {
+                System.out.println("Введите имя для поиска контакта:");
+                for (;;) {
+                    String searchName = scanner.nextLine();
+                    if(PhoneBook.inputNameOrNot(searchName)){
+                        System.out.println(phoneBook.getContactByName(searchName));
+                        break;
+                    } else {System.out.println("Неверный формат ввода");}
+                }
                 continue;
             }
 
             // КОД ПО ВВОДУ ИМЕНИ__________________________________________
-            if (PhoneBook.inputNameOrNot(firstInput)){ // если первый ввод - это имя и оно корректно:
-                if (phoneBook.checkName(firstInput)){ // если оно уже есть в книге
+            if (PhoneBook.inputNameOrNot(firstInput)) { // если первый ввод - это имя и оно корректно:
+                if (phoneBook.checkName(firstInput)) { // если оно уже есть в книге
                     System.out.println("Это имя уже есть в телефонной книге.");
                     System.out.println("Если хотите добавить номер телефона для абонента “" + firstInput +
                             "”, введите “ДОБАВИТЬ”");
                     System.out.println("Если не хотите добавлять, введите “НЕТ”");
-                    for (;;) {
+                    for (; ; ) {
                         String command = scanner.nextLine(); // ждём ввода
-                        if (command == "ДОБАВИТЬ") {
+                        if (command.equals("ДОБАВИТЬ")) {
                             System.out.println("Введите другой номер телефона для абонента “" + firstInput + "”:");
                             for (; ; ) {
                                 String secondInput = scanner.nextLine(); // просим ввести номер
                                 if (PhoneBook.inputPhoneOrNot(secondInput)) { // если номер корректен:
-                                    secondInput = PhoneBook.cleanPhone(secondInput); // приводим номер к дефолтному формату
-                                    phoneBook.addContact(secondInput, firstInput); // добавляем в книгу
-                                    System.out.println("Контакт сохранен!");
+                                    secondInput = PhoneBook.cleanPhone(secondInput); // приводим номер к формату
+                                    // проверяем нет ли в книге такого номера
+                                    if (phoneBook.checkNumber(secondInput)) { // если такой номер уже есть
+                                        System.out.println("Упс. Этот номер уже занят. " +
+                                                "Если хотите перезаписать абонента для номера “" + secondInput +
+                                                "“, введите “ПЕРЕЗАПИСАТЬ”");
+                                        System.out.println("Если не хотите перезаписывать, введите “НЕТ”");
+                                        for (; ; ) {
+                                            String command2 = scanner.nextLine(); // ждём ввода
+                                            if (command2.equals("ПЕРЕЗАПИСАТЬ")) {
+                                                phoneBook.addContact(secondInput, firstInput); // добавляем в книгу
+                                                System.out.println("Контакт сохранен!");
+                                                break;
+                                            } else if (command2.equals("НЕТ")) {
+                                                break;
+                                            } else {System.out.println("Неверный формат ввода");}
+                                        }
+                                    } else {phoneBook.addContact(secondInput, firstInput); // добавляем в книгу
+                                        System.out.println("Контакт сохранен!");}
                                     break;
-                                    // если номер некорректен:
-                                } else {System.out.println("Неверный формат ввода");}
-                            }break;
-                        // если введена команда "нет" - возвращаемся к началу цикла
-                        } else if (command == "НЕТ") {break;}
-                                // если ввод не "ДОБАВИТЬ" и не "НЕТ":
-                        else System.out.println("Введена неверная команда.");
+                                } else // если номер некорректен:
+                                {System.out.println("Неверный формат ввода");}
+                            } break;
+                            // если введена команда "нет" - возвращаемся к началу цикла
+                        } else if (command.equals("НЕТ")) {
+                            break;
+                        } else // если ввод не "ДОБАВИТЬ" и не "НЕТ":
+                        {System.out.println("Введена неверная команда.");}
                     }
+                    continue;
                 }
                 System.out.println("Такого имени в телефонной книге нет.");
                 System.out.println("Введите номер телефона для абонента “" + firstInput + "”:");
-                for (;;) {
+                for (; ; ) {
                     String secondInput = scanner.nextLine(); // просим ввести номер
                     if (PhoneBook.inputPhoneOrNot(secondInput)) { // если номер корректен:
                         secondInput = PhoneBook.cleanPhone(secondInput); // приводим номер к дефолтному формату
-                        phoneBook.addContact(secondInput, firstInput); // добавляем в книгу
-                        System.out.println("Контакт сохранен!");
+                        // проверяем нет ли в книге такого номера
+                        if (phoneBook.checkNumber(secondInput)) { // если такой номер уже есть
+                            System.out.println("Упс. Этот номер уже занят. " +
+                                    "Если хотите перезаписать абонента для номера “" + secondInput +
+                                    "“, введите “ПЕРЕЗАПИСАТЬ”");
+                            System.out.println("Если не хотите перезаписывать, введите “НЕТ”");
+                            for (; ; ) {
+                                String command2 = scanner.nextLine(); // ждём ввода
+                                if (command2.equals("ПЕРЕЗАПИСАТЬ")) {
+                                    phoneBook.addContact(secondInput, firstInput); // добавляем в книгу
+                                    System.out.println("Контакт сохранен!");
+                                    break;
+                                } else if (command2.equals("НЕТ")) {
+                                    break;
+                                } else {System.out.println("Неверный формат ввода");}
+                            }
+                        } else {phoneBook.addContact(secondInput, firstInput); // добавляем в книгу
+                            System.out.println("Контакт сохранен!");}
                         break;
                         // если номер некорректен:
                     } else {System.out.println("Неверный формат ввода");}
                 }
 
-            // КОД ПО ВВОДУ НОМЕРА________________________________________________
-            } else if (PhoneBook.inputPhoneOrNot(firstInput)){ // если первый ввод номер и он корректен
-                if (phoneBook.checkNumber(firstInput)){ // если номер уже есть в книге
+                // КОД ПО ВВОДУ НОМЕРА________________________________________________
+            } else if (PhoneBook.inputPhoneOrNot(firstInput)) { // если первый ввод номер и он корректен
+                if (phoneBook.checkNumber(firstInput)) { // если номер уже есть в книге
                     System.out.println("Этот номер уже есть в телефонной книге");
                     System.out.println("Если хотите перезаписать абонента для номера телефона “" + firstInput +
                             "”, введите “ПЕРЕЗАПИСАТЬ”");
                     System.out.println("Если не хотите перезаписывать, введите “НЕТ”");
-                    String command = scanner.nextLine(); // ждём ввода
-                    if (command == "ПЕРЕЗАПИСАТЬ"){
-                        System.out.println("Введите другое имя абонента для номера “" + firstInput + "”:");
-                        String secondInput = scanner.nextLine(); // просим ввести имя
-                        if (PhoneBook.inputNameOrNot(secondInput)){ // если имя корректно:
-                            phoneBook.addContact(firstInput,secondInput); // перезаписываем имя
-                            System.out.println("Контакт сохранен!");
-                            // если имя некорректно:
-                        } else {System.out.println("Неверный формат ввода");}
-                        // если введена команда "нет" - возвращаемся к началу цикла
-                    } else if (command == "НЕТ"){
-                        continue;
-                        // если ввод не "ДОБАВИТЬ" и не "НЕТ":
-                    } else System.out.println("Введена неверная команда.");continue;
-
+                    for (; ; ) {
+                        String command = scanner.nextLine(); // ждём ввода
+                        if (command.equals("ПЕРЕЗАПИСАТЬ")) {
+                            System.out.println("Введите другое имя абонента для номера “" + firstInput + "”:");
+                            for (; ; ) {
+                                String secondInput = scanner.nextLine(); // просим ввести имя
+                                if (PhoneBook.inputNameOrNot(secondInput)){ // если имя корректно:
+                                    phoneBook.addContact(firstInput, secondInput); // перезаписываем имя
+                                    System.out.println("Контакт сохранен!");
+                                    break;
+                                    // если имя некорректно:
+                                } else {System.out.println("Неверный формат ввода");}
+                            }
+                            break;
+                            // если введена команда "нет" - возвращаемся к началу цикла
+                        } else if (command.equals("НЕТ")){
+                            break;
+                            // если ввод не "ДОБАВИТЬ" и не "НЕТ":
+                        } else {System.out.println("Введена неверная команда.");}
+                    }
                 }
             } else {System.out.println("Неверный формат ввода");}
         }
-
     }
 }
--- src/main/java/practice/PhoneBook.java
+++ src/main/java/practice/PhoneBook.java
@@ -4,64 +4,92 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class PhoneBook {
-    HashMap <String,String> phoneBook = new HashMap<>();
-    String phone = "";
-    String name = "";
-
-   public static boolean inputNameOrNot (String str){
-       // регекс, проверяющий является ли ввод именем
-       String regexName = "[а-яА-Я]{2,}";
-       if(str.matches(regexName)){return true;} else return false;}
-    public static boolean inputPhoneOrNot (String str) {
-        // регекс, проверяющий является ли ввод номером телефона
-        String regexNumber = "[+]?[7,8][-(\s]?[0-9]{3}[-)\s]?[0-9]{3}[-]?[0-9]{2}[-]?[0-9]{2}";
-        if(str.matches(regexNumber)){return true;} else return false;}
-    public static String cleanPhone (String str){
-        // регекс, очищающий номер телефона до дефолта
-        String regexClean = "[^0-9]";
-        str = str.replaceAll(regexClean,"");
-        return str;
-    }
-    public String typePhoneBook () {
-        String type = "";
-        return type;
-    }
-    public boolean checkName (String str){
-        if (phoneBook.containsValue(str)){return true;} else return false;}
-    public boolean checkNumber (String str){
-        if (phoneBook.containsKey(str)){return true;} else return false;}
-
-    public void addContact(String phone, String name) {
-        // проверьте корректность формата имени и телефона
-        // (рекомедуется написать отдельные методы для проверки является строка именем/телефоном)
-        // если такой номер уже есть в списке, то перезаписать имя абонента
-    }
-
-    public String getContactByPhone(String phone) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найдены - вернуть пустую строку
-        return "";
-    }
-
-    public Set<String> getContactByName(String name) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найден - вернуть пустой TreeSet
-        return new TreeSet<>();
-    }
-
-    public Set<String> getAllContacts() {
-        // формат одного контакта "Имя - Телефон"
-        // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
-    }
-
-    // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
-    // это поможет вам найти все ключи (key) по значению (value)
+// проверьте корректность формата имени и телефона
+// (рекомедуется написать отдельные методы для проверки является строка именем/телефоном)
+// если такой номер уже есть в списке, то перезаписать имя абонента
+// для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
+// это поможет вам найти все ключи (key) по значению (value)
     /*
         for (Map.Entry<String, String> entry : map.entrySet()){
             String key = entry.getKey(); // получения ключа
             String value = entry.getValue(); // получения ключа
         }
     */
+public class PhoneBook {
+    HashMap <String,String> phoneBook = new HashMap<>();
+
+    public static boolean inputNameOrNot (String str){
+        // регекс, проверяющий является ли ввод именем
+        String regexName = "[а-яА-Я]{2,}";
+        return str.matches(regexName);}
+    public static boolean inputPhoneOrNot (String str) {
+        // регекс, проверяющий является ли ввод номером телефона
+        String regexNumber = "[+]?[7,8][-(\s]?[0-9]{3}[-)\s]?[0-9]{3}[-]?[0-9]{2}[-]?[0-9]{2}";
+        return str.matches(regexNumber);}
+    public static String cleanPhone (String str){
+        // регекс, очищающий номер телефона до дефолта
+        String regexClean = "[^0-9]";
+        str = str.replaceAll(regexClean,"");
+        return str;
+    }
+    public boolean checkName (String str){
+        return phoneBook.containsValue(str);}
+
+    public boolean checkNumber (String str){
+        return phoneBook.containsKey(str);}
+
+    public void addContact(String phone, String name) {
+        if (PhoneBook.inputPhoneOrNot(phone) & PhoneBook.inputNameOrNot(name)){
+            phoneBook.put(phone,name);}}
+
+    public String getContactByPhone(String phone) {
+        String value = phoneBook.get(phone); // получение значения
+        if (phoneBook.containsKey(phone)){
+            return value + " - " + phone;
+        } else return "";
+        // формат одного контакта "Имя - Телефон"
+        // если контакт не найдены - вернуть пустую строку
+    }
+
+    public TreeSet<String> getContactByName(String name) {
+        // формат одного контакта "Имя - Телефон"
+        // если контакт не найден - вернуть пустой TreeSet
+        TreeSet<String> contact = new TreeSet<>();
+        if (phoneBook.containsValue(name)){
+            for (HashMap.Entry<String, String> entry : phoneBook.entrySet()) {
+                String key = entry.getKey(); // получение ключа
+                String value = entry.getValue(); // получение значения
+                if (value.equals(name)){contact.add(value + " - " + key);}
+            }
+            return contact;
+        } else return new TreeSet<>();
+    }
+
+    public Set<String> getAllContacts() {
+        // формат одного контакта "Имя - Телефон"
+        // если контактов нет в телефонной книге - вернуть пустой TreeSet
+        if (!phoneBook.isEmpty()){ // если телефонная книга не пуста:
+            TreeSet <String> allContacts = new TreeSet<>();
+            HashMap <String,String> nameAggregator = new HashMap<>();
+
+            // переворачиваем ключи и значения в другой HashMap для того, чтобы
+            // можно было сконкатинировать номера телефонов в одну строчку
+            // если конкатинировать номера телефонов в одну строчку в методе add
+            // не будет работать поиск по номеру телефона
+            for (HashMap.Entry<String, String> entry : phoneBook.entrySet()) {
+                String number = entry.getKey(); // получение ключа
+                String name = entry.getValue(); // получение значения
+                if (!nameAggregator.containsKey(name)){
+                    nameAggregator.put(name,number);
+                } else nameAggregator.put(name,(nameAggregator.get(name) + ", " + number));
+            }
+            // переписываем перевернутый HashMap в TreeSet
+            for (HashMap.Entry<String, String> entry : nameAggregator.entrySet()) {
+                String name = entry.getKey(); // получение ключа
+                String number = entry.getValue(); // получение значения
+                allContacts.add(name + " - " + number);
+            }
+            return allContacts;
+        } else return new TreeSet<>();
+    }
 }
